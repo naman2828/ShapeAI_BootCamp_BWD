5fa1b73865eb2895f9b7bdecfe6b794045f44bdc
naman2828 <85469912+naman2828@users.noreply.github.com>
2021-06-07 08:50:03 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Header(){
+  return(
+    <header>
+      <h1>Naman Toshniwal</h1>
+    </header>  
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note(){
+  return(
+    <div className="note">
+      <h1>Nick Name</h1>
+      <p>NT</p>
+    </div>
+  );
+}
+export default Note;
